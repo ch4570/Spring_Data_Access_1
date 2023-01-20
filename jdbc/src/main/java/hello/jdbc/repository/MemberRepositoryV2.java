47ec79ef52c0f2804383f7e83de71f64b48dcffc
DevSeoRex <ch4570@naver.com>
2023-01-20 19:56:30 +0900

    스프링 데이터 접근기술 1 - 트랜잭션 적용 2 수강 & 실습 완료

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
@@ -11,11 +11,11 @@ import java.sql.*;
 import java.util.NoSuchElementException;
 
 /*
-*   JDBC - DataSource 사용, JdbcUtils 사용
+*   JDBC - Connection Param(Connection을 Parameter로 넘기는 예제)
 * */
 @Slf4j
 @RequiredArgsConstructor
-public class MemberRepositoryV1 {
+public class MemberRepositoryV2 {
 
     private final DataSource dataSource;
 
@@ -70,6 +70,35 @@ public class MemberRepositoryV1 {
         }
     }
 
+    public Member findById(Connection con, String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            // Connection은 여기서 닫지 않는다.
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(pstmt);
+        }
+    }
+
     public void update(String memberId, int money) throws SQLException{
         String sql = "update member set money=? where member_id=?";
 
@@ -88,6 +117,26 @@ public class MemberRepositoryV1 {
             throw e;
         } finally {
             close(con, pstmt, null);
+        }
+    }
+
+
+    public void update(Connection con, String memberId, int money) throws SQLException{
+        String sql = "update member set money=? where member_id=?";
+
+        PreparedStatement pstmt = null;
+
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            JdbcUtils.closeStatement(pstmt);
         }
     }
 
