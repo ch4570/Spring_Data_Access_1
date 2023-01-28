7910e51970ab109ddbe42e2cea901e5f03ad78eb
서창희 <>
2023-01-28 15:42:10 +0900

    스프링과 문제 해결 - 트랜잭션 수강& 실습 완료

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -7,32 +7,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
 import java.sql.SQLException;
 
 
 /*
-*   트랜잭션 - 트랜잭션 매니저
+*   트랜잭션 - 트랜잭션 템플릿
 * */
-@RequiredArgsConstructor
 @Slf4j
-public class MemberServiceV3_1 {
+public class MemberServiceV3_2 {
 
-    private final PlatformTransactionManager transactionManager;
+
     private final MemberRepositoryV3 memberRepository;
+    private final TransactionTemplate txTemplate;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
 
     public void accountTransfer(String fromId, String toId, int money){
-        // 트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
+        txTemplate.executeWithoutResult((status) -> {
             // 비즈니스 로직
-            bizLogic(fromId, toId, money);
-            // 성공시 커밋
-            transactionManager.commit(status);
-        } catch (Exception e) {
-            // 실패시 롤백
-            transactionManager.rollback(status);
-            throw new IllegalStateException(e);
-        }
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
